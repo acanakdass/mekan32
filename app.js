735f60a925c2fc5f6624f58df82a938e6360c0a9
Ahmet Can <acanakdas2@gmail.com>
2020-12-31 20:42:55 +0300

    updte

--- app.js
+++ app.js
@@ -48,9 +48,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening server on port 3000");
-  
+app.listen(3000, function () {
+  console.log('listening on port 3000!');
 });
 
 module.exports = app;
