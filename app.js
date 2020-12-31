59914ee6c66551c253ca560a2f377eda82d95148
Ahmet Can <acanakdas2@gmail.com>
2020-12-31 20:46:11 +0300

    last up

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, function () {
-  console.log('listening on port 3000!');
-});
+// app.listen(3000, function () {
+//   console.log('listening on port 3000!');
+// });
 
 module.exports = app;
